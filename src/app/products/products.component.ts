a3ff883f08e9a8d359cafe4078561d20398ee18c
ashishnaghate <ashishnaghate112@gmail.com>
2022-08-14 19:29:28 +0530

    paginator and delete refresh issue done

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { DataService } from '../data.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatSort, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 export interface Product {
   id: number;
@@ -17,37 +19,62 @@ export interface Product {
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-
-export class ProductsComponent implements AfterViewInit {
+export class ProductsComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  displayedColumns: string[] = ['id', 'name', 'description', 'price', 'quantity','actionsColumn'];
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'description',
+    'price',
+    'quantity',
+    'actionsColumn',
+  ];
   dataSource: any;
-  // dataSource = new MatTableDataSource<Product>();
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private _liveAnnouncer: LiveAnnouncer
+  ) {}
 
-
-  // dataSource = ELEMENT_DATA;
-  constructor(private dataService: DataService, private route: ActivatedRoute,
-    private router: Router,) { }
   ngOnInit(): void {
+   this.loadData();
+  }
+
+  loadData() {
     this.dataService.getProducts().subscribe((data: any) => {
-      console.log(data);
-      // this.dataSource = data;
-      // this.dataSource = new MatTableDataSource<Product>(data);
 
-    })
-
+      this.dataSource = new MatTableDataSource<Product>(data);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+    });
   }
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
   }
+
   deleteProduct(id) {
-    this.dataService.deleteProduct(id)
-      .subscribe(response => {
-        console.log(response);
-        this.router.navigate(['/products']);
-      })      
+    this.dataService.deleteProduct(id).subscribe((response) => {
+      console.log(response);
+      this.loadData();
+      // this.router.navigate(['/products']);
+    });
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: any) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
