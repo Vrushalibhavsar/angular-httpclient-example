fb1b1e7c816d19fb4f773699cc2b2a0cb519cec8
vrushalibhavsar <vrushalibhavsar13@gmail.com>
2022-09-19 18:33:51 +0530

    margin changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [ { path: '', redirectTo: 'home', pathMatch: 'full'},
 { path: 'home', component: HomeComponent },
 { path: 'about', component: AboutComponent },
 { path: 'products', component: ProductsComponent },
-{ path: 'add', component: AddComponent },];
+{ path: 'products/add', component: AddComponent },
+{ path: 'products/add/:id', component: AddComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
